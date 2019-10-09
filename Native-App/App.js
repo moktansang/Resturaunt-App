0ddf0a4fdee60a0b8ec13a2d0335afd185f22e7b
Jesse <jsainz237@gmail.com>
2019-10-09 00:22:26 -0500

    add checkout tab to App

--- Native-App/App.js
+++ Native-App/App.js
@@ -4,11 +4,13 @@ import { Text, View} from 'react-native';
 
 // APP
 import AppNavigator from './components/AppNavigator/AppNavigator'
+import Checkout from './components/Checkout/Checkout'
 
 /** This is the main component of the App */
 export default function App() {
   return (
     <View style={{ height: '100%', width: '100%' }}>
+      <Checkout />
       <AppNavigator />
     </View>
   );
