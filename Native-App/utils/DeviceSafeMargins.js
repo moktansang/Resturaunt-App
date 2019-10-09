ee0e6d07be887248e477d4e691762590978d2303
Jesse <jsainz237@gmail.com>
2019-10-09 00:15:24 -0500

    dimension utilities

---
+++ Native-App/utils/DeviceSafeMargins.js
@@ -0,0 +1,8 @@
+/**
+ * list of safe area margins per device
+ */
+const DeviceSafeMargins = {
+    iPad: { top: 24, bottom: 40, total: 44 },
+}
+
+export default DeviceSafeMargins;
---
+++ Native-App/utils/deviceDimensions.js
@@ -0,0 +1,5 @@
+import { Dimensions } from 'react-native'
+
+const device = Dimensions.get('window');
+
+export default device;
