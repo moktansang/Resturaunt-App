f20b3bbd23ad0bdfbb603d4b8f55ea9ccf9f19a1
Wassim Lagnaoui <lagnaouiw@gmail.com>
2019-10-20 22:15:11 -0500

    added The xcode project

---
+++ Rgino/Rgino/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Rgino
+//
+//  Created by WASSIM LAGNAOUI on 10/17/19.
+//  Copyright © 2019 WASSIM LAGNAOUI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func InPreparation(_ sender: Any) {
+          // go to the inPreparation page
+    }
+    
+    
+    @IBAction func compOrder(_ sender: Any) {
+        // go to view orders who are ready
+    }
+    
+    @IBAction func allOrders(_ sender: Any) {
+        // go to view all orders of the day
+    }
+    
+}
+
