5c64e25286ebf3dd7c5154995914b5c161140d94
Jesse <jsainz237@gmail.com>
2019-11-16 17:34:44 -0600

    See order items and total

--- Native-App/components/Checkout/Checkout.js
+++ Native-App/components/Checkout/Checkout.js
@@ -25,7 +25,7 @@ class Checkout extends React.Component {
         }
 
         this.animated_opacity = new Animated.Value(0);
-        this.animated_X_translation = new Animated.Value(-400);
+        this.animated_X_translation = new Animated.Value(-1000);
 
         this.startOpenAnimation = this.startOpenAnimation.bind(this);
         this.startCloseAnimation = this.startCloseAnimation.bind(this);
@@ -55,7 +55,7 @@ class Checkout extends React.Component {
         Animated.parallel([
             // animate moving order reviewer
             Animated.timing(this.animated_X_translation, {
-                toValue: -400,
+                toValue: -1000,
                 duration: 500,
                 easing: Easing.bezier(.39, .01, .59, 1)
             }),
--- Native-App/components/OrderReviewer/OrderReviewer.js
+++ Native-App/components/OrderReviewer/OrderReviewer.js
@@ -28,28 +28,46 @@ class OrderReviewer extends React.Component {
     async componentDidMount() {
         await Font.loadAsync({
             'Montserrat-Bold': require('../../assets/fonts/Montserrat/Montserrat-Bold.ttf'),
+            'Montserrat-SemiBold': require('../../assets/fonts/Montserrat/Montserrat-SemiBold.ttf'),
         })
         this.setState({ FontsLoaded: true });
     }
 
     renderOrderItems() {
         const items = this.props.orderItems.map((item, index) => {
+            const itemTotalPrice = (item.quantity * item.price).toFixed(2)
+            const spaceChar = itemTotalPrice >= 10.00 ? "" : " "
             return (
-                <Text key={index}>{`\u2022`}</Text>
+                <View style={styles.itemWrapper} key={index}>
+                    <Text style={{ fontFamily: 'Montserrat-SemiBold', ...styles.item }}>
+                        {`\u2022`} {item.name}
+                    </Text>
+                    <Text style={{ fontFamily: 'Montserrat-SemiBold', ...styles.item }}>
+                        x {item.quantity}  -  {spaceChar}${itemTotalPrice}
+                    </Text>
+                </View>
             )
         })
         return items;
     }
 
     render() {
-        const { animated_X_translation } = this.props;
+        const { animated_X_translation, orderItems } = this.props;
+        let totalPrice = 0;
+        orderItems.forEach(item => totalPrice += (item.quantity * item.price));
+        totalPrice = totalPrice.toFixed(2);
+
         return !this.state.FontsLoaded ? null : (
             <Animated.View style={{ height: Device.height, right: animated_X_translation ? animated_X_translation : 0, ...styles.orderReviewer }}>
                 <Text style={{ fontFamily: 'Montserrat-Bold', fontSize: 25 }}>Review your Order</Text>
-                <View style={styles.orderItems}>
-                    <ScrollView>
+                <View style={{ borderBottomColor: 'black', borderBottomWidth: 2, ...styles.orderItems }}>
+                    <ScrollView showsVerticalScrollIndicator >
                         {this.renderOrderItems()}
                     </ScrollView>
+                </View>
+                <View style={styles.totalWrapper}>
+                    <Text style={{ fontFamily: 'Montserrat-Bold', fontSize: 25 }}>Total:</Text>
+                    <Text style={{ fontFamily: 'Montserrat-Bold', fontSize: 25 }}>${totalPrice}</Text>
                 </View>
                 <OrderConfirmationButton />
             </Animated.View>
